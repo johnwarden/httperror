23703ef672c1a818b2f357159b4dcb5224e33efb
Jonathan R. Warden <john.warden@gmail.com>
2022-11-01 15:40:18 -0600

    PanicMiddleware takes optional shutdown function as second argument

--- panic.go
+++ panic.go
@@ -6,14 +6,24 @@ import (
 )
 
 // PanicMiddleware wraps a [httperror.Handler], returning a new [httperror.HandlerFunc] that
-// recovers from panics and returns them as errors.
-func PanicMiddleware(h Handler) HandlerFunc {
+// recovers from panics and returns them as errors. The second argument is an optional 
+// function that is called if there is a panic. This function can be used, for example, to
+// cleanly shutdown the server. 
+func PanicMiddleware(h Handler, s func()) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) (err error) {
+
 		defer func() {
 			if r := recover(); r != nil {
 				isErr := false
 				if err, isErr = r.(error); !isErr {
 					err = fmt.Errorf("%v", r)
+				}
+				if s != nil {
+					// the shutdown function must be called in a goroutine. Otherwise, if it is used
+					// to shutdown the server, we'll get a deadlock with the server shutdown function
+					// waiting for this request handler to finish, and this request waiting for the
+					// server shutdown function.
+					go s()
 				}
 			}
 		}()
@@ -24,14 +34,20 @@ func PanicMiddleware(h Handler) HandlerFunc {
 }
 
 // XPanicMiddleware wraps a [httperror.XHandler], returning a new [httperror.XHandlerFunc] that
-// recovers from panics and returns them as errors.
-func XPanicMiddleware[P any](h XHandler[P]) XHandlerFunc[P] {
+// recovers from panics and returns them as errors. The second argument is an optional 
+// function that is called if there is a panic. This function can be used, for example, to
+// cleanly shutdown the server. 
+func XPanicMiddleware[P any](h XHandler[P], s func()) XHandlerFunc[P] {
 	return func(w http.ResponseWriter, r *http.Request, p P) (err error) {
 		defer func() {
 			if r := recover(); r != nil {
 				isErr := false
 				if err, isErr = r.(error); !isErr {
 					err = fmt.Errorf("%v", r)
+				}
+				if s != nil {
+					// the shutdown function must be called in a goroutine. See comment in PanicMiddleware above.
+					go s()
 				}
 			}
 		}()
